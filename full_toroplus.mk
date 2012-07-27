87c923cbd235a2b90e5b6736af0638dfc75914c9
dho <dho@dho.im>
2012-07-27 18:30:15 -0400

    fix building/toroplus, again

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -27,8 +27,8 @@ PRODUCT_PACKAGES := \
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
-    device/samsung/toroplus/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/samsung/toroplus/plus-apns-conf.xml:system/etc/apns-conf.xml \
+    device/samsung/torospr/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/samsung/torospr/plus-apns-conf.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Inherit from those products. Most specific first.
@@ -36,4 +36,4 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit from toro device
-$(call inherit-product, device/samsung/toroplus/device.mk)
+$(call inherit-product, device/samsung/torospr/device.mk)
