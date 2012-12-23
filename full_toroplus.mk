3234e61e7b35731c88177662fb04446ca85c61db
nuclearmistake <nuclearmistake@gmail.com>
2012-12-23 11:28:11 -0500

    Fix Toroplus stuff
    Squashed commit of the following:
    
    commit b7e66c5ef18947829dfbd09db0d9a7eeeae2b24c
    Merge: 800c712 6a3a37b
    Author: DHO <DHO@dho.im>
    Date:   Wed Dec 19 21:59:47 2012 -0500
    
        Merger - Part 2 of the Toroplus overhaul
    
        Stevie forked our repo, got it working, and now forking it back.
    
        Change-Id: I49510c45547ec986b266dd53f8ccdf030ba873a9
    
    commit 800c712ddc9c9b46f652c00287cfee861a37f79d
    Author: StevieGore <Stevie_Gore@hotmail.com>
    Date:   Wed Dec 19 09:05:20 2012 -0500
    
        initial commit - fix
    
        Change-Id: I166e96c1307e53700df117068ac3cc21da61de49
    
    Change-Id: I166e96c1307e53700df117068ac3cc21da61de49

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -14,12 +14,16 @@
 
 #
 # This file is the build configuration for a full Android
-# build for maguro hardware. This cleanly combines a set of
+# build for toroplus hardware. This cleanly combines a set of
 # device-specific aspects (drivers) with a device-agnostic
 # product configuration (apps). Except for a few implementation
 # details, it only fundamentally contains two inherit-product
-# lines, full and toroplus, hence its name.
+# lines, full and maguro, hence its name.
 #
+
+#CDMATools
+PRODUCT_PACKAGES += \
+    CDMATools
 
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/samsung/toroplus/test-apns-conf_sprint.xml:system/etc/apns-conf.xml
@@ -34,6 +38,13 @@ $(call inherit-product, device/samsung/toroplus/device.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_toroplus
 PRODUCT_DEVICE := toroplus
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full AOSP on Toroplus
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mysidspr \
+    BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.1.1/JRO03U/L700FH05:user/release-keys" \
+    PRIVATE_BUILD_DESC="mysidspr-user 4.1.1 JRO03U L700FH05 release-keys"
