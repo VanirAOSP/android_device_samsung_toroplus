365ee9228afa83d31d22e62c6e815c2cf0ffeb6b
DHOMD <DHO@dho.im>
2013-09-14 11:07:14 -0400

    TOROPLUS_RADIO
    Recent Merge remote-tracking branch 'cm/cm-10.2' into HEAD now requires this
    
    Change-Id: I4fb38eae9e8d3237e8356492a1240bae525bbae3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,7 @@ BOARD_INVENSENSE_APPLY_COMPASS_NOISE_FILTER := true
 
 # libsec-ril_lte.so is broke due to a missing symbol in surfacecomposerclient
 TARGET_TOROPLUS_RADIO_FIX := true
+TARGET_TOROPLUS_RADIO := true
 
 #kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/tuna
